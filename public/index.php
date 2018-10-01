62923216308b46bfd6c28d1d87b6c0a8f1e41ae1
El hachemi AGGOUN <42415109+Hisham67@users.noreply.github.com>
2018-10-01 17:20:30 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: etienne
- * Date: 28/09/18
- * Time: 09:22
- */
-
 
 require '../vendor/autoload.php';
 require '../vendor/ehime/hello-world/src/HelloWorld/SayHello.php';
